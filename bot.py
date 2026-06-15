7bfdad8074281a76cb1d85fbfc586e27d6530daf
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-15 23:40:49 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+@bot.command(name="티켓생성")
+@commands.has_permissions(administrator=True)
 async def t_create_panel(ctx):
 
     file = discord.File("price.png", filename="price.png")
@@ -7,7 +9,7 @@ async def t_create_panel(ctx):
         description=(
             "상담, 구매 진행, 문의사항이 있으시다면\n"
             "아래 📩 버튼을 눌러주세요!\n\n"
-            "📌 아래 가격표를 먼저 확인해주세요."
+            "📌 구매 전 가격표를 확인해주세요."
         ),
         color=0x5865F2
     )
