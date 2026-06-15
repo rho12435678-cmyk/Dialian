40a8690f3d925ea9d616ff3521ae7d118ca0f736
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-15 23:34:23 +0900

    Update dial.py

--- dial.py
+++ dial.py
@@ -662,21 +662,6 @@ async def auto_announce():
 @commands.has_permissions(administrator=True)
 async def t_create_panel(ctx):
 
-    @bot.command(name="가격표")
-async def price(ctx):
-    channel = bot.get_channel(PRICE_CHANNEL_ID)
-
-    file = discord.File("price.png", filename="price.png")
-
-    embed = discord.Embed(
-        title="🎨 Dial GFX Hub 가격표",
-        color=0x5865F2
-    )
-
-    embed.set_image(url="https://cdn.discordapp.com/attachments/1396537248018731020/1516085715740393612/file_0000000047c47206840f4e48fc0c0f9d_.png?ex=6a315c5a&is=6a300ada&hm=b8ccdadedfac2b65efa9a2d80ad781483b892c9a04d1e03bf407570bca173689&")
-
-    await channel.send(file=file, embed=embed)
-    
     embed = discord.Embed(
         title="💼 커미션 및 문의 상담 공간",
         description=(
@@ -690,6 +675,22 @@ async def price(ctx):
         embed=embed,
         view=TicketOpenView()
     )
+
+
+@bot.command(name="가격표")
+async def price(ctx):
+    channel = bot.get_channel(PRICE_CHANNEL_ID)
+
+    embed = discord.Embed(
+        title="🎨 Dial GFX Hub 가격표",
+        color=0x5865F2
+    )
+
+    embed.set_image(
+        url="https://cdn.discordapp.com/attachments/1396537248018731020/1516085715740393612/file_0000000047c47206840f4e48fc0c0f9d_.png"
+    )
+
+    await channel.send(embed=embed)
 
 # ==================== [봇 시작 시스템] ====================
 
