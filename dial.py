a560fdd43a5ebf3cb7a4b91187f67177b2723f2f
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-16 00:14:52 +0900

    Update dial.py

--- dial.py
+++ dial.py
@@ -661,8 +661,6 @@ async def auto_announce():
 @commands.has_permissions(administrator=True)
 async def t_create_panel(ctx):
 
-    file = discord.File("price.png", filename="price.png")
-
     embed = discord.Embed(
         title="💼 커미션 및 문의 상담 공간",
         description=(
