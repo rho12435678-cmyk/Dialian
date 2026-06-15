b3ed74141b31d05ab704459f1bef11e99a58b3fd
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-15 23:48:57 +0900

    Update dial.py

--- dial.py
+++ dial.py
@@ -673,13 +673,14 @@ async def t_create_panel(ctx):
         color=0x5865F2
     )
 
-    embed.set_image(url="file_0000000047c47206840f4e48fc0c0f9d_.png")
+    embed.set_image(
+    url="https://cdn.discordapp.com/attachments/1396537248018731020/1516085715740393612/file_0000000047c47206840f4e48fc0c0f9d_.png"
+)
 
-    await ctx.send(
-        file=file,
-        embed=embed,
-        view=TicketOpenView()
-    )
+await ctx.send(
+    embed=embed,
+    view=TicketOpenView()
+)
 
 # ==================== [봇 시작 시스템] ====================
 
