f793165ad2a9238ca6182dcfa6d4ddfd6f9fe097
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-28 01:05:20 +0900

    Update designer_select.py

--- database/designer_select.py
+++ database/designer_select.py
@@ -15,30 +15,29 @@ class DesignerSelect(discord.ui.Select):
             options.append(
                 discord.SelectOption(
                     label=name,
-                    value=dev_id
+                    value=str(dev_id)
                 )
             )
 
         super().__init__(
-            placeholder="담당 GFX 디자이너를 선택해주세요.",
+            placeholder="담당 GFX 디자이너를 선택하세요.",
+            min_values=1,
+            max_values=1,
             options=options
         )
 
     async def callback(self, interaction: discord.Interaction):
 
         modal = PurchaseModal()
-
         modal.selected_designer = int(self.values[0])
 
         await interaction.response.send_modal(modal)
 
 
-class DesignerSelectView(discord.ui.View):
+class DesignerView(discord.ui.View):
 
     def __init__(self):
 
-        super().__init__(timeout=300)
+        super().__init__(timeout=180)
 
-        self.add_item(
-            DesignerSelect()
-        )
+        self.add_item(DesignerSelect())
