d9f8ea53d225ddd5835ce84381547c0a96c825c5
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-27 22:54:04 +0900

    Update ticket_view.py

--- database/modals/views/ticket_view.py
+++ database/modals/views/ticket_view.py
@@ -1,5 +1,10 @@
 import discord
 
+from modals.gfx_modal import PurchaseModal
+from modals.uniform_modal import UniformModal
+from modals.logo_modal import LogoModal
+
+
 class CommissionSelect(discord.ui.Select):
 
     def __init__(self):
@@ -7,44 +12,54 @@ class CommissionSelect(discord.ui.Select):
         options = [
 
             discord.SelectOption(
-                label="Roblox GFX",
-                emoji="🎨",
-                description="GFX 커미션 신청",
-                value="gfx"
+                label="🎨 Roblox GFX",
+                value="gfx",
+                description="GFX 커미션 신청"
             ),
 
             discord.SelectOption(
-                label="Roblox Uniform",
-                emoji="👕",
-                description="복장 커미션 신청",
-                value="uniform"
+                label="👕 Roblox Uniform",
+                value="uniform",
+                description="복장 커미션 신청"
             ),
 
             discord.SelectOption(
-                label="Discord Logo",
-                emoji="🖌",
-                description="로고 커미션 신청",
-                value="logo"
+                label="🖌 Discord Logo",
+                value="logo",
+                description="로고 커미션 신청"
             )
 
         ]
 
         super().__init__(
             placeholder="커미션 종류를 선택해주세요.",
+            min_values=1,
+            max_values=1,
             options=options
         )
 
     async def callback(self, interaction: discord.Interaction):
 
-        await interaction.response.send_message(
-            f"선택한 커미션 : **{self.values[0]}**",
-            ephemeral=True
-        )
+        if self.values[0] == "gfx":
+            await interaction.response.send_modal(
+                PurchaseModal()
+            )
+
+        elif self.values[0] == "uniform":
+            await interaction.response.send_modal(
+                UniformModal()
+            )
+
+        elif self.values[0] == "logo":
+            await interaction.response.send_modal(
+                LogoModal()
+            )
 
 
-class TicketSelectView(discord.ui.View):
+class TicketOpenView(discord.ui.View):
 
     def __init__(self):
+
         super().__init__(timeout=None)
 
         self.add_item(
