83003cbf6ad792230e854a294f63097421d8ba6f
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-28 02:01:10 +0900

    Update review_view.py

--- database/views/review_view.py
+++ database/views/review_view.py
@@ -131,7 +131,7 @@ class StarRatingView(discord.ui.View):
                             )
                         )
 
-                                                await db.commit()
+                        await db.commit()
 
                 success_view = discord.ui.View()
 
