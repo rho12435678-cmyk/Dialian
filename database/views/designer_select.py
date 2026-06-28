a82b4260aae0b2d35305300cbc0fbedabb29a2a2
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-28 17:57:49 +0900

    Update designer_select.py

--- database/views/designer_select.py
+++ database/views/designer_select.py
@@ -2,8 +2,6 @@ import discord
 
 from config import DESIGNERS
 from database.modal.gfx_modal import PurchaseModal
-from database.modal.logo_modal import LogoModal
-from database.modal.uniform_modal import UniformModal
 
 
 class DesignerSelect(discord.ui.Select):
@@ -17,7 +15,7 @@ class DesignerSelect(discord.ui.Select):
             member = guild.get_member(dev_id)
 
             if member:
-                label = member.display_name      # 서버 별명
+                label = member.display_name  # 서버 별명
             else:
                 label = f"알 수 없는 디자이너 ({dev_id})"
 
@@ -35,15 +33,10 @@ class DesignerSelect(discord.ui.Select):
             options=options
         )
 
-        
-async def callback(self, interaction: discord.Interaction):
+    async def callback(self, interaction: discord.Interaction):
 
-    modal = PurchaseModal()
-    modal.selected_designer = int(self.values[0])
-
-    await interaction.response.send_modal(modal)
-
-        modal.selected_designer = designer_id
+        modal = PurchaseModal()
+        modal.selected_designer = int(self.values[0])
 
         await interaction.response.send_modal(modal)
 
