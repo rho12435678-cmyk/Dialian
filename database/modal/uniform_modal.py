9dd87bfeb1241f7132f5e3c0305f667e86cb280b
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-28 18:44:56 +0900

    Update uniform_modal.py

--- database/modal/uniform_modal.py
+++ database/modal/uniform_modal.py
@@ -1,23 +1,14 @@
-from database.modal.gfx_modal import PurchaseModal
+import discord
 
+class UniformModal(discord.ui.Modal, title="👕 Roblox 복장 커미션 신청서"):
 
-class UniformModal(PurchaseModal):
+    content = discord.ui.TextInput(
+        label="원하는 복장 내용을 작성해주세요",
+        style=discord.TextStyle.paragraph,
+        placeholder="원하는 복장 디자인을 자유롭게 작성해주세요.",
+        required=True,
+        max_length=1000
+    )
 
-    MODAL_TITLE = "👕 Roblox 복장 커미션 신청서"
-
-    FORM_TITLE = "📋 Roblox 복장 커미션 신청서"
-    FIELD1 = "👕 복장 이름"
-    FIELD2 = "🎽 복장 종류"
-    FIELD3 = "🎨 원하는 복장 스타일"
-
-    def __init__(self):
-        super().__init__()
-
-        self.roblox_nickname.label = "복장 이름"
-        self.roblox_nickname.placeholder = "예: 경찰 제복"
-
-        self.gfx_type.label = "복장 종류"
-        self.gfx_type.placeholder = "예: 경찰 / 군복 / 카페"
-
-        self.gfx_style.label = "원하는 복장 스타일"
-        self.gfx_style.placeholder = "예: 검정색, 금색 포인트, 현실풍"
+    async def on_submit(self, interaction: discord.Interaction):
+        # 여기서 티켓 생성
