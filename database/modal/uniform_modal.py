9f2e239572794b056219578f947549260cf315a4
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-28 19:12:31 +0900

    Update uniform_modal.py

--- database/modal/uniform_modal.py
+++ database/modal/uniform_modal.py
@@ -1,26 +1,10 @@
 import discord
 
-from database.modal.gfx_modal import PurchaseModal
+from database.modal.simple_ticket_modal import SimpleTicketModal
 
 
-class UniformModal(PurchaseModal):
+class UniformModal(SimpleTicketModal):
 
     MODAL_TITLE = "👕 Roblox 복장 커미션 신청서"
     FORM_TITLE = "📋 Roblox 복장 커미션 신청서"
-
-    FIELD1 = "👕 복장 내용"
-    FIELD2 = "➖"
-    FIELD3 = "➖"
-
-    def __init__(self):
-        super().__init__()
-
-        self.roblox_nickname.label = "원하는 복장 내용을 작성해주세요"
-        self.roblox_nickname.placeholder = "원하는 디자인, 색상, 참고사항 등을 자유롭게 작성해주세요."
-        self.roblox_nickname.max_length = 1000
-
-        self.gfx_type.required = False
-        self.gfx_type.default = "-"
-
-        self.gfx_style.required = False
-        self.gfx_style.default = "-"
+    FIELD_NAME = "👕 원하는 복장 내용"
