12149063deb9b7185ff1bc58d23ec8f6dae381de
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-28 19:06:33 +0900

    Update logo_modal.py

--- database/modal/logo_modal.py
+++ database/modal/logo_modal.py
@@ -1,17 +1,26 @@
 import discord
 
-class LogoModal(discord.ui.Modal, title="🖌 로고 커미션 신청서"):
+from database.modal.gfx_modal import PurchaseModal
 
-    content = discord.ui.TextInput(
-        label="원하는 로고 내용을 작성해주세요",
-        style=discord.TextStyle.paragraph,
-        placeholder="원하는 색상, 분위기, 참고사항 등을 자유롭게 작성해주세요.",
-        required=True,
-        max_length=1000
-    )
 
-    async def on_submit(self, interaction: discord.Interaction):
-        await interaction.response.send_message(
-            "로고 신청이 접수되었습니다.",
-            ephemeral=True
-        )
+class LogoModal(PurchaseModal):
+
+    MODAL_TITLE = "🖌 로고 커미션 신청서"
+    FORM_TITLE = "📋 로고 커미션 신청서"
+
+    FIELD1 = "🖌 로고 내용"
+    FIELD2 = "➖"
+    FIELD3 = "➖"
+
+    def __init__(self):
+        super().__init__()
+
+        self.roblox_nickname.label = "원하는 로고 내용을 작성해주세요"
+        self.roblox_nickname.placeholder = "원하는 색상, 분위기, 참고사항 등을 자유롭게 작성해주세요."
+        self.roblox_nickname.max_length = 1000
+
+        self.gfx_type.required = False
+        self.gfx_type.default = "-"
+
+        self.gfx_style.required = False
+        self.gfx_style.default = "-"
