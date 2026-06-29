1a2faa1fc7e25c5600c7de3082dd4ed6c1ec56ef
rho12435678-cmyk <rho12435678@gmail.com>
2026-06-29 22:02:52 +0900

    Update DailyNotice.py

--- database/views/database/DailyNotice.py
+++ database/views/database/DailyNotice.py
@@ -12,7 +12,7 @@ class DailyNotice:
         self.bot = bot
         self.daily_notice.start()
 
-    @tasks.loop(time=time(hour=21, minute=39, tzinfo=KST))
+    @tasks.loop(time=time(hour=22, minute=4, tzinfo=KST))
     async def daily_notice(self):
 
         print("공지 실행")
