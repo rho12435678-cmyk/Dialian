cb1013e906bb262e68b1ef0862a76d87c3e660e0
rho12435678-cmyk <rho12435678@gmail.com>
2026-07-08 17:17:59 +0900

    Delete database/modal/logo_modal.py

--- database/modal/logo_modal.py
+++
@@ -1,8 +0,0 @@
-from database.modal.simple_ticket_modal import SimpleTicketModal
-
-
-class LogoModal(SimpleTicketModal):
-
-    MODAL_TITLE = "🖌 로고 커미션 신청서"
-    FORM_TITLE = "📋 로고 커미션 신청서"
-    FIELD_NAME = "🖌 원하는 로고 내용"
